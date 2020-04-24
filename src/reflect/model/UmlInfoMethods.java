9a6d5fa0bc3f360be84f621e3c436f060c35eb96
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-04-24 12:56:13 -0600

    methods to sort the information

--- src/reflect/model/UmlInfoMethods.java
+++ src/reflect/model/UmlInfoMethods.java
@@ -85,3 +85,125 @@ public class UmlInfoMethods {
         }
         return message;
     }
+
+    /**
+     * Sorts a list that is of type Executable
+     * @param list the list you want sorted
+     * @return the sorted array, or an array of size 0 if the list is not of type Executable,
+     *  or the list if its size is less then or equal to 1
+     */
+    private static Executable[] sortByParameters(Executable[] list)
+    {
+        if(list.length <= 1)
+        {
+            return list;
+        }
+        Executable pivot = list[0];
+        List<Executable> first = new ArrayList<>();
+        List<Executable> last = new ArrayList<>();
+
+        for (int index = 1; index < list.length; index++) {
+            Executable current = list[index];
+            if (current.getParameterCount() <= pivot.getParameterCount()) {
+                first.add(current);
+            } else if (current.getParameterCount() > pivot.getParameterCount()) {
+                last.add(current);
+            }
+        }
+        Executable[] middle = {pivot};
+
+        Executable[] firstArray = new Executable[first.size()];
+        Executable[] lastArray = new Executable[last.size()];
+        first.toArray(firstArray);
+        lastArray = last.toArray(lastArray);
+
+        return (Executable[]) combineArrays((Executable[]) combineArrays( sortByParameters(firstArray), middle), sortByParameters(lastArray));
+    }
+
+    private static Executable[] sortByName(Executable[] list) {
+        if (list.length <= 1) 
+            return list;
+        int half = list.length / 2;
+        Executable[] first = new Executable[half];//list.length/2];
+        Executable[] second = new Executable[half + list.length % 2];
+        //splits the array in half
+        for (int index = 0; index < list.length; index++) 
+            if (index < half) 
+                first[index] = list[index];
+            else 
+                second[index - half] = list[index];
+        first = sortByName(first);
+        second = sortByName(second);
+
+        // compare the lists and make them one in the correct order.
+        int indexList = 0;
+        int indexFirst = 0;
+        int indexSecond = 0;
+        while(indexList < list.length)
+        {
+            if(indexFirst >= first.length)
+            {
+                list[indexList] = second[indexSecond];
+                indexSecond++;
+            }
+            else if(indexSecond >= second.length)
+            {
+                list[indexList] = first[indexFirst];
+                indexFirst++;
+            }
+            else
+            {
+                if (first[indexFirst].getName().compareTo(second[indexSecond].getName()) < 0) 
+                {
+                    list[indexList] = first[indexFirst];
+                    indexFirst++;
+                } 
+                else if (first[indexFirst].getName().compareTo(second[indexSecond].getName()) > 0) 
+                {
+                    list[indexList] = second[indexSecond];
+                    indexSecond++;
+                }
+                else
+                {
+                    list[indexList] = first[indexFirst];
+                    indexFirst++;
+                    indexList++;
+                    list[indexList] = second[indexSecond];
+                    indexSecond++;
+                }
+            }
+            indexList++;
+        }
+        return list;
+    }
+
+    /**
+     * combines two arrays after making sure they are the same type. 
+     * adds the second array to the first array
+     * @param firstList the list that is first in the new list
+     * @param lastList the list that starts after the first list
+     * @return the new list as an object which needs to be cast or a Object array with 0 elements
+     */
+    public static Object combineArrays(Object[] firstList, Object[] lastList)
+    {
+        Class cl1 = firstList.getClass();
+        Class cl2 = lastList.getClass();
+        Class componentType1 = cl1.getComponentType();
+
+        //cl1.isInstance(cl2)
+        if(cl1.getComponentType().getName().contentEquals(cl2.getComponentType().getName()))
+        {
+            int newLength = firstList.length + lastList.length;
+            Object newArray = Array.newInstance(componentType1, newLength);
+            System.arraycopy(firstList, 0, newArray,0,firstList.length);
+            System.arraycopy(lastList, 0, newArray, firstList.length, lastList.length);
+
+            return newArray;
+        }
+        else
+        {
+            Object emptyArray = Array.newInstance(componentType1, 0);
+            return emptyArray;
+        }
+    }
+}
