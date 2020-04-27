c7984393d0c3bdcf35d8f5fc59e014c189786ced
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-04-26 23:45:39 -0600

    made a test case just to see if the program will get all the information
    can also be used to debug the program

--- src/reflect/controller/UmlController.java
+++ src/reflect/controller/UmlController.java
@@ -10,11 +10,26 @@ public class UmlController {
 	{
 		this.gui = new ReflectFrameChoices(this);
 	}
-
+	
 	public String getClassInfo(String className, boolean[] options) {
 		return UmlInfoMethods.getClassInfo(className, options);
 	}
 	
+	
+	public void start()
+	{
+		displayMessage(getClassInfo("java.util.ArrayList", null));
+	}
+	
+	private void displayMessage(String message)
+	{
+		System.out.println(message);
+	}
+	private String askQuestion(String message)
+	{
+		System.out.println(message);
+		return "";
+	}
 	// have it be able to sort info by modifiers then name
 	// how to make code to do this
 }
--- src/reflect/controller/UmlRunner.java
+++ src/reflect/controller/UmlRunner.java
@@ -2,5 +2,6 @@ package reflect.controller;
 public class UmlRunner {
     public static void main(String[] args) {
     	UmlController app = new UmlController();
+    	app.start();
     }
 }
