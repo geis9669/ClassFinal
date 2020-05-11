91f0720db761d77d28bb8dcf10b5404c919b98a4
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-05-11 14:22:54 -0600

    start of a way to save an image

---
+++ src/reflect/controller/UMLCanvas.java
@@ -0,0 +1,14 @@
+package reflect.controller;
+
+import java.awt.*;
+
+public class UMLCanvas extends Canvas
+{
+	
+	public void paint(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+	}
+}
