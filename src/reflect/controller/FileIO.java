e53cfc492a0f9e61bfe989f7be10f2c920cf3d9f
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-05-11 15:30:58 -0600

    added doc to a method

--- src/reflect/controller/FileIO.java
+++ src/reflect/controller/FileIO.java
@@ -6,6 +6,12 @@ import java.util.*;
 
 public class FileIO {
 
+	/**
+	 * saves text to a text file
+	 * @param app the controller of the program
+	 * @param path where to save the file on your system
+	 * @param textToSave the text that you are saving
+	 */
 	public static void saveText(UmlController app, String path, String textToSave)
 	{
 		String filename = path;
