84ae185c880327bb5b6828fd707c260bd85fb063
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-05-16 22:40:48 -0600

    added java document

--- src/reflect/model/SortBy.java
+++ src/reflect/model/SortBy.java
@@ -1,5 +1,17 @@
 package reflect.model;
 
+/**
+ * this is used to specifiy a sort
+ * @author Greg
+ *
+ * @param <T> the type that you want to sort
+ */
 public interface SortBy<T> {
+	/**
+	 * how you want the two items sorted
+	 * @param item1 the first item
+	 * @param item2 the second item
+	 * @return zero is equal, the others are determined how you want and what you use this with
+	 */
 	public int sortBy(T item1,T  item2);
 }
